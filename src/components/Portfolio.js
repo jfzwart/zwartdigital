f8e4d1cf6c2c704797152c27e3b27468623bba7d
Jeroen Zwart <jeroen.zwart@live.com>
2021-02-26 19:19:10 +0100

    corrected portfolio object

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Item1 from '../assets/website.jpg'
 
-const items = () => {
-    return [
+const items = [
         {id:1,title:'Streams', desc: "Twitch Like Streaming Platform", lang: ["React", "JS", "HTML", "SCSS"], code: "https://github.com/jfzwart/Streams", link: "", img: Item1},
         {id:2,title:'Watchbuddy', desc: "Social Stream Recommendation Service", lang: ["Ruby", "JS", "HTML", "SCSS"], code: "https://github.com/Gerro385/watchbuddy", link: "", img: Item1},
         {id:3,title:'Widgets', desc: "Widgets", lang: ["React", "JS", "HTML", "SCSS"], code: "https://github.com/jfzwart/widgets", link: "", img: Item1},
@@ -12,8 +11,14 @@ const items = () => {
         {id:7,title:'Youtube API', desc: "React Hooks Video Tool", lang: ["React", "JS", "HTML", "SCSS"], code: "https://github.com/jfzwart/videos", link: "", img: Item1},
         {id:8,title:'Zwart Portfolio', desc: "Portfolio Website", lang: ["React", "JS", "HTML", "SCSS"], code: "https://github.com/jfzwart/zwartportfolio", link: "", img: Item1},
         {id:9,title:'AirBnB Clone', desc: "Airbnb Like Website Offering Fancy Vehicles", lang: ["Ruby", "JS", "HTML", "SCSS"], code: "https://github.com/kasia-gwc/AirBnB_for_Cars", link: "", img: Item1},
-    ]
-}
+];
+
+const itemList = items.map(item => {
+    return (
+       console.log(item)
+    )
+})
+
 
 const Portfolio = () => {
     return (
