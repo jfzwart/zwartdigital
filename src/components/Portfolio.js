82db3d38ba5e744998672cf65c038a6cc7701a8f
Jeroen Zwart <jeroen.zwart@live.com>
2021-03-18 16:49:46 +0100

    redone styling portolio page with material ui

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,9 +7,48 @@ import Item5 from '../assets/shopping.png';
 import Item6 from '../assets/youtube.png';
 import Item7 from '../assets/wehicle.png';
 
-import '../stylesheets/portfolio.scss';
-import { faCode } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import '../stylesheets/portfolio.scss';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import Container from '@material-ui/core/Container';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        padding: 30,
+        paddingBottom: 60,
+    },
+    card: {
+        minWidth: 225,
+    },
+    media: {
+        height: 0,
+        paddingTop: "56.25%" // 16:9
+    },
+    expand: {
+        transform: "rotate(0deg)",
+        marginLeft: "auto",
+        transition: theme.transitions.create("transform", {
+            duration: theme.transitions.duration.shortest
+        })
+    },
+    expandOpen: {
+        transform: "rotate(180deg)"
+    },
+
+}));
 
 
 const items = [
@@ -25,32 +64,69 @@ const items = [
         {id:9,title:'AirBnB Clone', desc: "Airbnb Like Website Offering Fancy Vehicles", lang: ["Ruby", "JS", "HTML", "SCSS"], code: "https://github.com/kasia-gwc/AirBnB_for_Cars", link: "https://wehicle.herokuapp.com/", img: Item7},
 ];
 
-const itemList = items.map(item => {
-
-    return (
-        <div className="card" key={item.id} >
-            <div className="card-image">
-                <a href={item.link} alt={item.title}>
-                    <img src={item.img} alt={item.title} />
-                </a>
-                <a href={item.code}>
-                    <button className="btn btn-success">
-                    <FontAwesomeIcon icon={faCode} />
-                    </button> 
-                </a>
-            </div>
-        </div>
-    )
-})
-
-
 const Portfolio = () => {
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    const itemList = items.map(item => {
+
+        return (
+            <Grid item xs={12} sm={6} m={3}> 
+                <Card className={classes.card}>
+                <CardHeader title={item.title} />
+                <CardMedia
+                    className={classes.media}
+                    image={item.img}
+                    title="Paella dish"
+                />
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                    {item.desc}
+                    </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                    <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                    </IconButton>
+                    <IconButton aria-label="share">
+                    <ShareIcon />
+                    </IconButton>
+                    <IconButton
+                    className={clsx(classes.expand, {
+                        [classes.expandOpen]: expanded
+                    })}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                    >
+                    <ExpandMoreIcon />
+                    </IconButton>
+                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                    <Typography paragraph>Method:</Typography>
+                    <Typography paragraph>
+                        Heat 1/2 cup of the broth in a pot until simmering, add saffron and
+                        set aside for 10 minutes.
+                    </Typography>
+                    </CardContent>
+                </Collapse>
+                </Card>
+            </Grid>
+            );
+    })
+
     return (
-        <div className="container">
-        <div className="box">
-            {itemList}
-        </div>
-    </div>
+        <React.Fragment> 
+            <Container maxWidth="m">
+                <Grid container className={classes.root} spacing={6} >
+                        {itemList}
+                </Grid>
+            </Container>
+        </React.Fragment>
     )
 };
 
